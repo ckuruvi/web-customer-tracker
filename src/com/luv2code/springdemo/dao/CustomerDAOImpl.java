5198ed734ae4694bee5ac82b217479cb7d2ac1e4
ckuruvi <charleskuruvila@yahoo.com>
2017-03-30 21:39:46 -0500

    CRUD operations using Spring & Hibernate

---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,86 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	
+	//need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	
+	@Override
+	public List<Customer> getCustomers() {
+		
+		//get hibernate session
+		Session currentSession=sessionFactory.getCurrentSession();
+		
+		//create a query.. sort by last name
+		Query<Customer> theQuery=
+				currentSession.createQuery("from Customer order by lastName",
+						Customer.class);
+		
+		//excute query and  get result list
+		List<Customer> customers=theQuery.getResultList();
+		
+		//return the results
+		
+		return customers;
+	}
+
+
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		
+		//get hibernate session
+		Session currentSession= sessionFactory.getCurrentSession();
+		
+		currentSession.saveOrUpdate(theCustomer);
+		
+		//save the customer
+		
+		
+		
+	}
+
+
+
+	@Override
+	public Customer getCustomer(int theId) {
+		
+		Session currentSession =sessionFactory.getCurrentSession();
+		
+		Customer theCustomer = currentSession.get(Customer.class,theId);
+		return theCustomer;
+	}
+
+
+
+	@Override
+	public void deleteCustomer(int theId) {
+		System.out.println("********Customer Id ****************"+theId);
+		//get the current hibernate session
+		Session currentSession =sessionFactory.getCurrentSession();
+		
+		//delete the object with the primary key
+		Query theQuery=
+		currentSession.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+		theQuery.executeUpdate();
+	}
+
+}
